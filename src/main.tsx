30d92c445733153faa36d0f02a633c7ac555b6cc
Sergei Kultyshkin <kultyshkin2003@gmail.com>
2025-12-09 19:08:47 +0300

    add(main): базовая инициализация чистого проект со страницей Home

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Home } from './screens/home/Home'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+)
